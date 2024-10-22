e1fa7fa2a2fabc75cfd1bb40302ee1a7c1811301
Baptiste Pierrat <baptiste.pierrat@gmail.com>
2024-10-22 23:52:13 +0200

    xvfb

--- utils.py
+++ utils.py
@@ -131,6 +131,9 @@ import ufl
 import numpy as np
 import pyvista
 
+# If we're running headless, we need to start a virtual display
+if pyvista.OFF_SCREEN:
+    pyvista.start_xvfb()
 
 from mpi4py import MPI
 
